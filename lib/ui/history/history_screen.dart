172a54257a97f772bb5998375f6f25d969dc1a4f
ductintin <buiductienpy206@gmail.com>
2024-03-06 18:56:58 +0700

    history screen

--- lib/ui/history/history_screen.dart
+++ lib/ui/history/history_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:mvvm_flutter_app/data/model/api/response/history_response.dart';
 import 'package:mvvm_flutter_app/ui/history/history_view_model.dart';
+import 'package:mvvm_flutter_app/utils/number_utils.dart';
 import 'package:provider/provider.dart';
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -11,8 +14,6 @@ class HistoryScreen extends StatefulWidget {
 
 class _HistoryScreenState extends State<HistoryScreen> {
   final ScrollController scrollController = ScrollController();
-  List<HistoryResponse> products = [];
-  HistoryViewModel viewModel = HistoryViewModel();
 
 
   @override
@@ -20,12 +21,20 @@ class _HistoryScreenState extends State<HistoryScreen> {
     // TODO: implement initState
     super.initState();
     scrollController.addListener(loadMore);
-    viewModel.getHistory();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    scrollController.dispose();
 
   }
   @override
   Widget build(BuildContext context) {
     final vm = Provider.of<HistoryViewModel>(context);
+
+    HistoryViewModel historyViewModel = context.watch<HistoryViewModel>();
 
     return Scaffold(
       body: SafeArea(
@@ -64,147 +73,9 @@ class _HistoryScreenState extends State<HistoryScreen> {
                   ),
                 ),
                 SizedBox(height: 15),
-                SizedBox(
-                  child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    controller: scrollController,
-                    itemCount: 1,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Card(
-                          surfaceTintColor: Colors.white,
-                          shadowColor: Colors.lightGreenAccent,
-                          elevation: 4.0,
-                          child: Padding(
-                            padding: EdgeInsets.all(16.0),
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      '25/09/2023 15:00',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w500
-                                      ),
-                                    ),
 
-                                    SizedBox(
-                                      width: 30,
-                                    ),
+                _ui(historyViewModel),
 
-                                    Expanded(
-                                      child: Text(
-                                        '27.000 đ',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 16
-                                        ),
-                                      ),
-                                    ),
-
-                                    Image(image: AssetImage('assets/images/icon_green_dot.png')),
-
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-
-                                    Text(
-                                      'Hoàn thành',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w700,
-                                        color: Colors.green,
-                                      ),
-                                    )
-                                  ],
-                                ),
-
-                                SizedBox(
-                                  height: 10,
-                                ),
-
-                                Row(
-                                  children: [
-                                    Image(
-                                      image: AssetImage('assets/images/icon_vector.png'),
-                                      height: 20,
-                                      width: 20,
-                                    ),
-
-                                    SizedBox(
-                                      width: 15,
-                                    ),
-
-                                    Flexible(
-                                      child: Text(
-                                        '150/17 Đinh Tiên Hoàng, Phường26, Q.3, Hoss',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 16,
-                                          color: Colors.grey,
-                                          overflow: TextOverflow.ellipsis
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-
-                                ),
-
-                                SizedBox(
-                                  height: 10,
-                                ),
-
-                                Row(
-                                  children: [
-                                    Image(
-                                      image: AssetImage('assets/images/icon_destination.png'),
-                                      height: 20,
-                                      width: 20,
-                                    ),
-
-                                    SizedBox(
-                                      width: 15,
-                                    ),
-
-                                    Flexible(
-                                      child: Text(
-                                        'Masteri Thảo Điền, Phường 2, Q.2, Ho Chi Minh',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 16,
-                                            color: Colors.grey,
-                                            overflow: TextOverflow.ellipsis
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-
-                                ),
-
-                                SizedBox(
-                                  height: 10,
-                                ),
-
-                                Container(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    'AllBike',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 16
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                )
               ],
             ),
           ],
@@ -214,6 +85,154 @@ class _HistoryScreenState extends State<HistoryScreen> {
 
   }
 
+  _ui(HistoryViewModel historyViewModel){
+    if(historyViewModel.isLoading){
+      return Container();
+    }
+    return Expanded(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        physics: const AlwaysScrollableScrollPhysics(),
+        shrinkWrap: true,
+        controller: scrollController,
+        itemCount: historyViewModel.histories.length,
+        itemBuilder: (context, index) {
+          HistoryResponse history = historyViewModel.histories[index];
+
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              surfaceTintColor: Colors.white,
+              shadowColor: Colors.lightGreenAccent,
+              elevation: 4.0,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          NumberUtils.formatDate(history.createdDate!),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+
+                        SizedBox(
+                          width: 30,
+                        ),
+
+                        Expanded(
+                          child: Text(
+                            NumberUtils.formatMoneyToString(history.money!) + " đ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16
+                            ),
+                          ),
+                        ),
+
+                        Image(image: AssetImage(history.state == 300 ? 'assets/images/icon_green_dot.png' : (history.state == -100 ? 'assets/images/icon_red_dot.png' : 'assets/images/icon_yellow_dot.png'))),
+
+                        SizedBox(
+                          width: 10,
+                        ),
+
+                        Text(
+                          history.state == 300 ? 'Hoàn thành' : (history.state == -100 ? 'Đã hủy' : 'Đang thực hiện'),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: history.state == 300 ? Colors.green : (history.state == -100 ? Colors.red : Colors.yellow),
+                          ),
+                        )
+                      ],
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                    Row(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/icon_vector.png'),
+                          height: 20,
+                          width: 20,
+                        ),
+
+                        SizedBox(
+                          width: 15,
+                        ),
+
+                        Flexible(
+                          child: Text(
+                            history.pickupAddress ?? '',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16,
+                                color: Colors.grey,
+                                overflow: TextOverflow.ellipsis
+                            ),
+                          ),
+                        ),
+                      ],
+
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                    Row(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/icon_destination.png'),
+                          height: 20,
+                          width: 20,
+                        ),
+
+                        SizedBox(
+                          width: 15,
+                        ),
+
+                        Flexible(
+                          child: Text(
+                            history.destinationAddress ?? '',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16,
+                                color: Colors.grey,
+                                overflow: TextOverflow.ellipsis
+                            ),
+                          ),
+                        ),
+                      ],
+
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'AllBike',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
   void loadMore(){
 
   }

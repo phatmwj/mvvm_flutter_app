6890f4b6e2cc33b3785850dec9feed6536ab2a5b
ductintin <buiductienpy206@gmail.com>
2024-05-15 20:57:16 +0700

    change launcher icon and app name

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,8 +7,14 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+   FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+      }
     GMSServices.provideAPIKey("AIzaSyAQWUevZCTLaVd9a1Z2WEA2_e2gO9iW8rU")
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
